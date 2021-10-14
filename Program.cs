726bc0ada444512c17a8b6b5932c838b8f97283e
Michael Aggerholm <92aggerholm@gmail.com>
2021-10-14 20:31:28 +0200

    Added POS files

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace POS_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new SchoolContext())
+            {
+
+                var std = new Student()
+                {
+                    Name = "Jack"
+                };
+
+                var crs = new Course()
+                {
+                    CourseName = "DatabaseProgrammering"
+                };
+
+                context.Students.Add(std);
+                context.Courses.Add(crs);
+                context.SaveChanges();
+            }
+        }
+    }
+}
